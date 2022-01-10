b3da5f2fc7a98660c98f153bb71c276e1ae4ea2e
Virginie Bonhomme <virginiebonhomme@Virginies-MBP.home>
2022-01-10 13:37:26 -0500

    index.js done

---
+++ hov-app/service/apiConfig/index.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const BASE = process.env.REACT_APP_AIRTABLE_BASE
+const KEY = process.env.REACT_APP_AIRTABLE_KEY
+const baseURL = `https://api.airtable.com/v0/${BASE}/Bars`
+
+
+const api = axios.create({
+  baseURL,
+  headers: {
+    Authorization: `Bearer ${KEY}`
+  }
+
+
+
+})
+
+export default api
