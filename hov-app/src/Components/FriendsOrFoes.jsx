0a4156cbe7dbed679855d31a672e165dacb1b57e
Virginie Bonhomme <virginiebonhomme@Virginies-MBP.home>
2022-01-18 14:43:22 -0500

    change casing

---
+++ hov-app/src/Components/FriendsOrFoes.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from 'react'
+import api from '../services/apiConfig/index.js'
+import Form from './Form'
+import Header from './Header.jsx'
+
+
+const defaultInput = {
+  category: "",
+  song: "",
+  album: "",
+  lyrics: ""
+}
+
+export default function FriendsOrFoes() {
+  const [bars, setBars] = useState([])
+  const [input, setInput] = useState(defaultInput)
+  const [toggle, setToggle] = useState(false)
+
+
+
+  useEffect(() => {
+    const fetchLyrics = async () => {
+      const res = await api.get();
+      const filteredBars = res.data.records.filter(bars => {
+        return bars.fields.category === "Friends or Foes"
+      })
+      setBars(filteredBars);
+    };
+    fetchLyrics();
+  }, [toggle]);
+
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const fields = input;
+    await api.post("", { fields });
+    setInput(defaultInput);
+    setToggle(prevToggle => !prevToggle)
+  };
+
+  const handleTextInput = (event) => {
+    const { name, value } = event.target;
+    setInput((prevInput) => ({
+      ...prevInput,
+      [name]: value,
+      category: "Friends or Foes"
+    }));
+  };
+
+  return (
+    <div className='friends-container'>
+      <Header
+        title="Hov-Said-What?"
+        button="Home"
+        route="/"
+      />
+      <h2>Friends or Foes</h2>
+      <div className='paragraph-container'>
+        <p>The ablilty to move in a room full of vultures is not only a skill but can also be life saving. Here are some bars to keep you on your toes. </p>
+      </div>
+      <div className='main-lyric-container'>
+        {bars.map((bar) => {
+          return (
+            <div key={bar.id} className='lyrics-container'>
+              <div>
+                <h4>{bar.fields?.lyrics}</h4>
+                <h4>Album: {bar.fields?.album}</h4>
+                <h4>Song: {bar.fields?.song}</h4>
+                <br />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className='form-container'>
+        <Form
+          input={input}
+          handleTextInput={handleTextInput}
+          handleSubmit={handleSubmit}
+          type={"Submit"}
+        />
+      </div>
+    </div>
+  )
+}
